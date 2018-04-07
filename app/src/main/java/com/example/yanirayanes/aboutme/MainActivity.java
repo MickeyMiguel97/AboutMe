34dd330daa1cc62c5e3a61db3cbab309ae088ab1
MickeyMiguel97 <00145016@uca.edu.sv>
2018-04-06 21:29:20 -0600

    Manda imagen y texto

--- app/src/main/java/com/example/yanirayanes/aboutme/MainActivity.java
+++ app/src/main/java/com/example/yanirayanes/aboutme/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.yanirayanes.aboutme;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void process (View view){
+
+        Intent intent = null, choose = null;
+        Uri imageUri = Uri.parse("android.resource://com.example.yanirayanes.aboutme/drawable/"+R.drawable.me);
+        intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_STREAM, imageUri);
+        intent.putExtra(Intent.EXTRA_TEXT, "Miguel Ernesto González\n"+"Ingenieria Informática\n\nFacebook: Miguel Ernesto González\nWhatsapp: 6420 - 9082\nGmail: 00145016@uca.edu.sv\nGithub: MickeyMiguel97\nG+: me.gyanes@gmail.com\nInstagram: @mickey.miguel\nYouTube: Miguel E. Yanes G.\nTwitter: @MiguelEYanesG1\n\nPOWERED BY MSSY");
+        choose = Intent.createChooser(intent,"Send Image");
+        startActivity(choose);
     }
 }
